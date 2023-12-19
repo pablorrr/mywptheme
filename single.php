af021de008bf17360b820fe1c07c7802a1d2d2f9
Paweł Kali <pkalisz451@gmail.com>
2023-12-19 14:19:09 +0100

    comments fix

--- single.php
+++ single.php
@@ -13,15 +13,16 @@ get_header();?>
         <div class="row mx-auto m-single">
             <section id="primary" class="content-area col-sm-12 col-md-8 col-lg-8">
                 <main id="main" class="site-main" role="main">
-                    <?php while (have_posts()) : the_post();//loop start
-                        get_template_part('template-parts/content');
-                     //   if (is_single() && comments_open()) {
-                          //  comments_template('', true);
-                      //  }  ?>
-                    <?php endwhile;//lopp finish ?>
+                    <!-- loop start -->
+                    <?php while (have_posts()): the_post(); ?>
+                      <?php get_template_part('template-parts/content');?>
+                      <?php if (is_single() && comments_open()): ?>
+                        <?php  comments_template('', true);?>
+                     <?php endif;?>
+                    <?php endwhile ; ?>
                 </main><!--#main-->
             </section><!--#primary-->
-            <?php get_sidebar(); ?>
+          
         </div><!--.row .mx-auto .m-single -->
     </div><!--.container-->
 <?php get_footer(); ?>
